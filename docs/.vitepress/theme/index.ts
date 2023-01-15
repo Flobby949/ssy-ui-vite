0feb2dc746fda3881f48e7ccdfc243fe789902ca
jinchenxing <2541226493@qq.com>
2023-01-15 16:58:18 +0800

    fix(custom): 修改文档配置

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,16 @@
-import Theme from 'vitepress/dist/client/theme-default'
-import SSYUI from '../../../src/entry'
+import DefaultTheme from 'vitepress/theme'
 
-// 主题样式
-import 'vitepress-theme-demoblock/theme/styles/index.css'
-// 插件的组件，主要是demo组件
-import Demo from 'vitepress-theme-demoblock/components/Demo.vue'
-import DemoBlock from 'vitepress-theme-demoblock/components/DemoBlock.vue'
+import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
+import Demo from 'vitepress-theme-demoblock/dist/client/components/Demo.vue'
+import DemoBlock from 'vitepress-theme-demoblock/dist/client/components/DemoBlock.vue'
+import SSYUI from '../../../src/entry'
+import 'uno.css'
 
 export default {
-    ...Theme,
-    enhanceApp({ app }) {
-        app.use(SSYUI)
-        app.component('Demo', Demo)
-        app.component('DemoBlock', DemoBlock)
-    }
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.use(SSYUI)
+    app.component('Demo', Demo)
+    app.component('DemoBlock', DemoBlock)
+  }
 }
